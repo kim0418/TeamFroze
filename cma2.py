85cbe9c869f80d334ba3f5ea9cb9843ffe2cec51
kim0418 <59432564+kim0418@users.noreply.github.com>
2023-11-06 20:52:58 +0900

    Update cma2.py

--- cma2.py
+++ cma2.py
@@ -1,6 +1,7 @@
 import cv2
 
-cap = cv2.VideoCapture(14)  # /dev/video14 장치를 사용
+cap = cv2.VideoCapture(0)  # /dev/video14 장치를 사용
+cap.set(cv2.CAP_PROP_FPS,30)
 if cap.isOpened():
     ret, frame = cap.read()
     if ret:  # 비디오 캡처가 제대로 되었는지 확인
