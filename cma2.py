9d26bbb28b2ab7c19e474434665c31d6e8727897
kim0418 <59432564+kim0418@users.noreply.github.com>
2023-11-06 20:47:51 +0900

    Update cma2.py

--- cma2.py
+++ cma2.py
@@ -1,11 +1,15 @@
 import cv2
 
-cap = cv2.VideoCapture(0) #0,14,15,21,22
+cap = cv2.VideoCapture(14)  # /dev/video14 장치를 사용
 if cap.isOpened():
     ret, frame = cap.read()
-    cv2.imshow('Captured Image', frame)
-    cv2.waitKey(0)  # 사용자가 키를 누를 때까지 기다립니다.
-    cv2.destroyAllWindows()  # 모든 창을 닫습니다.
+    if ret:  # 비디오 캡처가 제대로 되었는지 확인
+        cv2.imshow('Captured Image', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print('Failed to capture video.')
+        cap.release()  # 비디오 소스를 닫습니다.
 else:
-    print('Unable to open video source: /dev/video0')
+    print('Unable to open video source: /dev/video14')
 cap.release()
