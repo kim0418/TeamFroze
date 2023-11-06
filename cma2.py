31a33ae1e24336873f67f254f790d7f28322dbb4
kim0418 <59432564+kim0418@users.noreply.github.com>
2023-11-06 20:57:49 +0900

    Update cma2.py

--- cma2.py
+++ cma2.py
@@ -2,6 +2,8 @@ import cv2
 
 cap = cv2.VideoCapture(0)  # /dev/video14 장치를 사용
 cap.set(cv2.CAP_PROP_FPS,30)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1296)  # 프레임 너비를 1296으로 설정
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 972)
 if cap.isOpened():
     ret, frame = cap.read()
     if ret:  # 비디오 캡처가 제대로 되었는지 확인
