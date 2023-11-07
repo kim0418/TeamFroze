f62dc8d7b34d3fd024729389a50a9fdf5d40a2e9
kim0418 <59432564+kim0418@users.noreply.github.com>
2023-11-07 14:13:59 +0900

    Update cma2.py

--- cma2.py
+++ cma2.py
@@ -1,6 +1,7 @@
 import cv2
 
 cap = cv2.VideoCapture(0)  # /dev/video14 장치를 사용
+cv2.imshow(cap)
 cap.set(cv2.CAP_PROP_FPS,30)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1296)  # 프레임 너비를 1296으로 설정
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 972)
