a0200db585c1a503d471a6afa54875155b181273
kim0418 <59432564+kim0418@users.noreply.github.com>
2023-11-13 19:48:54 +0900

    Update tem.py

--- tem.py
+++ tem.py
@@ -19,6 +19,7 @@
 #             sumT=0.0
 
 
+import cv2
 import Adafruit_DHT as dht
 import time
 #from /usr.src.app.lights
@@ -29,6 +30,8 @@ sumT = 0.0
 
 while True:
     if timea != int(time.time()):
+        if(cv2.waitKey(1) % 0xFF==27):
+            break
         humidity, temperature = dht.read_retry(dht.DHT22, 4)
         tm = time.localtime(time.time())
         #print("Temperature={0:0.1f}*C    Humidity={1:0.1f}%    time={2:d}:{3:d}:{4:d}({5:d}/{6:d})".format(temperature, humidity, tm.tm_hour + 9, tm.tm_min, tm.tm_sec, tm.tm_mon, tm.tm_mday))
